c330061c24a357fded461613e4dff3351a7bf742
Camilo Loaiza <46464898+CloaizaF@users.noreply.github.com>
2026-07-28 20:48:16 -0500

    Add cookie authentication and visual testing

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Home Page", () => {
+test.describe("Home Page with no auth", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
+  });
+
+  test("Visual Test", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+    await expect(page).toHaveScreenshot("home-page-no-auth.png", {
+      mask: [page.getByTitle("Practice Software Testing - Toolshop")],
+    });
   });
 
   test("Check sign in", async ({ page }) => {
@@ -33,5 +40,23 @@ test.describe("Home Page", () => {
     await page.getByTestId("search-submit").click();
     await expect(productGrid.getByRole("link")).toHaveCount(1);
     await expect(page.getByAltText("Thor Hammer")).toBeVisible();
+  });
+});
+
+test.describe("Home Page Customer 01 auth", () => {
+  test.use({ storageState: ".auth/customer01.json" });
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
+
+  test("Visual Test Authorized", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+    await expect(page).toHaveScreenshot("home-page-customer01.png", {
+      mask: [page.getByTitle("Practice Software Testing - Toolshop")],
+    });
+  });
+
+  test("Check Customer 01 is signed in", async ({ page }) => {
+    await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
   });
 });
